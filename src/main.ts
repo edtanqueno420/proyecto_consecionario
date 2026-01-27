9f4011426512a4506e8e1d4b251276d0297cef73
edtanqueno420 <edison.tanqueno@gmail.com>
2026-01-26 20:38:07 -0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-import * as path from 'path';
 
 async function bootstrap() {
-  // =======================
-  // DIAGNÓSTICO .env
-  // =======================
-  const rutaArchivo = path.join(process.cwd(), '.env');
-  console.log('\n==================================================');
-  console.log('DIAGNÓSTICO DE INICIO:');
-  console.log('Buscando archivo en:', rutaArchivo);
-
-  if (fs.existsSync(rutaArchivo)) {
-    console.log('✅ ¡EL ARCHIVO .env EXISTE!');
-    const contenido = fs.readFileSync(rutaArchivo, 'utf8');
-    if (contenido.includes('DB_PASS')) {
-      console.log('✅ El archivo contiene la variable DB_PASS.');
-    } else {
-      console.log('❌ El archivo existe pero NO tiene la variable DB_PASS.');
-    }
-  } else {
-    console.log('❌ ERROR: No se encuentra el archivo .env');
-    console.log('⚠️  Revisa que no sea ".env.txt"');
-  }
-  console.log('==================================================\n');
-
-  // =======================
-  // APP
-  // =======================
   const app = await NestFactory.create(AppModule);
 
-  // ✅ CORS (OBLIGATORIO PARA EL FRONTEND)
+  // ✅ CORS CORRECTO PARA FRONTEND (Vite + Azure)
   app.enableCors({
-    origin:[
+    origin: [
       'http://localhost:5173'
     ],
-     // Vite
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`🚀 Backend corriendo en http://localhost:${process.env.PORT ?? 3000}`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`🚀 Backend iniciado correctamente`);
 }
 
 bootstrap();
